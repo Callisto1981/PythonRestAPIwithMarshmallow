07a175ee402135ef6b14be1ff3347bcad6f2dc64
Wayne <wabbruscatoorr@gmail.com>
2021-03-09 19:48:40 -0500

    Created Product database model

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
+import marshmallow as marshmallow
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import marshmallow
 import os
 
 # Init app
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({'msg': "Hello World"})
+
+# Initialize db
+db = SQLAlchemy(app)
+
+
+# Initialize marshmallow
+ma = marshmallow(app)
+
+
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
 
 
 # Run Server
